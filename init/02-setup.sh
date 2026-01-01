9bb32290dddc40582429b2c5a06596f955360c42
IIBendyII <115938680+IIBendyII@users.noreply.github.com>
2026-01-01 18:24:05 +0100

    Update and rename setup.sh to 02-setup.sh
    Bugfix e separazione inizializzazione tabella in 01-init.sql

---
+++ init/02-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+LOG_API_PASSWORD=$(cat /run/secrets/mysql_api_password)
+
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" <<-EOSQL
+-- Creazione Utente API
+CREATE USER IF NOT EXISTS 'api'@'%' IDENTIFIED BY '$LOG_API_PASSWORD';
+
+-- Assegnazione di soli privilegi di scrittura
+GRANT INSERT ON \`Logs\` TO 'api'@'%';
+GRANT INSERT ON \`Statistiche\` TO 'api'@'%';
+
+FLUSH PRIVILEGES;
+EOSQL
