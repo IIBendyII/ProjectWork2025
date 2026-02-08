aa696152f837a1671556cbc262085b4472b6b8b5
Riccardo <riccardo.bandi.studio@fitstic-edu.com>
2026-02-09 00:59:58 +0100

    hardening dei container

--- api/api_logger.py
+++ api/api_logger.py
@@ -14,34 +14,33 @@ from time import sleep
 import sys, os, random  # random da rimuovere a fine testing
 
 def prendisegreto(secretFile: str) -> str:
-    """Funzione che prende un segreto nella cartella dei docker secrets"""
+    """Funzione che prende un segreto dalla cartella dei docker secrets"""
     try:
-        #with open(f"run/secrets/{secretFile}", "r") as f:
-        with open(f"secrets/{secretFile}", "r") as f: # da sostituire a fine testing
+        with open(f"/run/secrets/{secretFile}", "r") as f:
+        #with open(f"secrets/{secretFile}", "r") as f: # da sostituire a fine testing
             return f.read().strip()
     except Exception as e:
         sys.exit(f"Errore nella lettura del segreto, eccezione: {e}")
 
-DB_LS_USER = "api"
-DB_GS_USER = prendisegreto("db_gs_api_user.txt")
-DB_LS_PASS = prendisegreto("db_ls_api_password.txt")
-DB_GS_PASS = prendisegreto("db_gs_api_password.txt")
-#DB_LS_HOST = os.getenv("DB_HOST","worldfit_db")
-DB_LS_HOST = "172.17.0.1" # da sostituire a fine testing
-DB_GS_HOST = prendisegreto("db_gs_ip.txt")
-DB_LS_PORT = "3306"
-DB_GS_PORT = "3306"
-DB_LS_NAME = "WorldFitLS"
-DB_GS_NAME = "WorldFit"
-DATABASE_LS_URI = f"mysql+pymysql://{DB_LS_USER}:{DB_LS_PASS}@{DB_LS_HOST}:{DB_LS_PORT}/{DB_LS_NAME}"
-DATABASE_GS_URI = f"mysql+pymysql://{DB_GS_USER}:{DB_GS_PASS}@{DB_GS_HOST}:{DB_GS_PORT}/{DB_GS_NAME}"
-ENCRYPTKEY = load_encrypt_key(prendisegreto("encrypt_key.txt"))
-ENCRYPTPAD = prendisegreto("pseudo_pad.txt")
+DB_LS_USER = os.getenv("DB_LS_USER")
+DB_GS_USER = prendisegreto("db_gs_api_user")
+DB_LS_PASS = prendisegreto("db_ls_api_password")
+DB_GS_PASS = prendisegreto("db_gs_api_password")
+DB_LS_HOST = os.getenv("DB_LS_HOST")
+#DB_LS_HOST = "172.17.0.1:3306" # da toglire a fine testing
+DB_GS_HOST = prendisegreto("db_gs_ip")
+DB_LS_NAME = os.getenv("DB_LS_DATABASE")
+DB_GS_NAME = os.getenv("DB_GS_DATABASE")
+DATABASE_LS_URI = f"mysql+pymysql://{DB_LS_USER}:{DB_LS_PASS}@{DB_LS_HOST}/{DB_LS_NAME}"
+DATABASE_GS_URI = f"mysql+pymysql://{DB_GS_USER}:{DB_GS_PASS}@{DB_GS_HOST}/{DB_GS_NAME}"
+ENCRYPTKEY = load_encrypt_key(prendisegreto("encrypt_key"))
+ENCRYPTPAD = prendisegreto("pseudo_pad")
+API_KEY = prendisegreto("api_key")
 
 if __name__ == "__main__":
     smartIDs = [] # non carico id su github, ve li inserite voi ;)
     for smartID in smartIDs:
-        pseudoSmartID = pseudonimizzatore(smartID, encryptKey=ENCRYPTKEY, encryptPad=ENCRYPTPAD)
+        pseudoSmartID = pseudonimizzatore(smartID, encrypt_key=ENCRYPTKEY, pseudo_pad=ENCRYPTPAD)
         palestraID = random.randint(1,50)
         timestamp = datetime.now()
         print(f"SmartCardID Generato: {smartID}, SmartCardID pseudonimizzato: " \
@@ -67,5 +66,3 @@ if __name__ == "__main__":
         # inserisco i dati anonimizzati nella tabella statistiche
         magazziniere.insert_stats(sesso=dati["sesso"], fascia_eta=dati["fascia_eta"], palestra_id=dati["palestra_id"],
                                   data_ingresso=dati["data_ingresso"], fascia_oraria=dati["fascia_oraria"])
-        sleep(1)
-        
--- db/init/02-setup.sh
+++ db/init/02-setup.sh
@@ -6,11 +6,11 @@ LOG_API_PASSWORD=$(cat /run/secrets/db_ls_api_password)
 
 mysql -u root -p"$ROOT_PASSWORD" "$MYSQL_DATABASE" <<-EOSQL
 -- Creazione Utente API
-CREATE USER IF NOT EXISTS 'api'@'%' IDENTIFIED BY '$LOG_API_PASSWORD';
+CREATE USER IF NOT EXISTS '$MYSQL_API_USER'@'%' IDENTIFIED BY '$LOG_API_PASSWORD';
 
 -- Assegnazione di soli privilegi di scrittura
-GRANT INSERT ON \`Logs\` TO 'api'@'%';
-GRANT INSERT ON \`Statistiche\` TO 'api'@'%';
+GRANT INSERT ON \`Logs\` TO '$MYSQL_API_USER'@'%';
+GRANT INSERT ON \`Statistiche\` TO '$MYSQL_API_USER'@'%';
 
 FLUSH PRIVILEGES;
 EOSQL
