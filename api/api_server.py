b234bc5ecc579eb4e3cfd6e7951476c3db1b516b
Riccardo <riccardo.bandi.studio@fitstic-edu.com>
2026-02-22 00:42:48 +0100

    implementazione beta dell'API

--- api/api_server.py
+++ api/api_server.py
@@ -12,7 +12,7 @@ handler = logging.StreamHandler(stdout)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 
 def prendisegreto(secretFile: str) -> str:
     """Funzione che prende un segreto dalla cartella dei docker secrets"""
@@ -36,102 +36,79 @@ ENCRYPTKEY = load_encrypt_key(prendisegreto("encrypt_key"))
 ENCRYPTPAD = prendisegreto("pseudo_pad")
 API_KEY = prendisegreto("api_key")
 
+DATETIME_FORMAT = r"%Y-%m-%d %H:%M:%S"
+HEXDIGITS = '0123456789ABCDEF'
 SECONDI_ACCETTABILI = 10
 
-def timestampCheck(ricezione):
-    differenza = timeDifferenceCalculator(timestamp, ricezione)
-    if differenza <= SECONDI_ACCETTABILI:
-        logger.debug("timestamp TRUE")
-        return True
-    else:
-        logger.debug("timestamp FALSE")
-        return False
-
-def signatureCheck():
-    string = str(idSmartCard) + str(idPalestra) + str(timestamp) + API_KEY
-    hashResult = hashing(string)
-    logger.debug(hashResult)
-
-    if hashResult == signature:
-        logger.debug("hash TRUE")
-        return True
-    else:
-        logger.debug("hash FALSE")
-        return False
-
-def palestraIdCheck(gestore):
+def timestampCheck(ricezione: datetime, timestamp: datetime) -> bool:
     '''
-    Dobbiamo mettere un controllo per verificare che idPalestra sia effettivamente un intero.
-    Non ha senso perdere tempo ad interrogare il gestore se idPalestra non è un numero.
-    Inoltre è un controllo di sanificazione, passabile come Defense in Depth.
+        Funzione che controlla il timestamp della richiesta ricevuta.
+        Se la ricezione è avvenuta entro un delta di secondi accettabile restituisce vero,
+        altrimenti falso.
+    '''
+    if int((ricezione - timestamp).total_seconds()) <= SECONDI_ACCETTABILI:
+        return True
+    else:
+        return False
+
+def signatureCheck(idSmartCard:str, idPalestra:int, timestamp:datetime, signature:str) -> bool:
+    '''Funzione che controlla la validità della firma ricevuta nella richiesta'''
+    string = idSmartCard + str(idPalestra) + timestamp.strftime(DATETIME_FORMAT) + API_KEY
+    hash = sha256(string.encode('utf-8')).hexdigest()
+    if hash == signature:
+        return True
+    else:
+        return False
+
+def palestraIdCheck(gestore:Gestionale, idPalestra:int) -> bool:
+    '''
+        Funzione che controlla che l'ID palestra ricevuto 
+        sia presente nella tabella delle palestre del DB Gestionale
     '''
     pal = gestore.select_palestra(idPalestra)
     if pal is not None:
-        logger.debug("pID TRUE")
         return True
     else:
-        logger.debug("pID FALSE")
         return False
 
-def cardIdCheck(gestore):
+def cardIdCheck(gestore:Gestionale, idSmartCard:str) -> bool:
     '''
-    Dobbiamo mettere un controllo per verificare che idSmartCard sia una stringa composta da 
-    6 caratteri esadecimali (quindi valori da 0 a 9 e dalla A alla F maiuscole).
-    Non ha senso perdere tempo ad interrogare il gestore se idSmartCard non è un id valido.
-    Inoltre è un controllo di sanificazione, passabile come Defense in Depth.
+        Funzione che controlla che lo SmartCard ID ricevuto 
+        sia presente nella tabella dei clienti del DB Gestionale
     '''
     card = gestore.select_client(idSmartCard)
     if card is not None:
-        logger.debug("cID TRUE")
         return True
-    else:        
-        logger.debug("cID FALSE")
+    else:
         return False
 
-def check(gestore, ricezione):
+def check(gestore: Gestionale, ricezione:datetime,
+          idSmartCard:str, idPalestra:int, timestamp:datetime, signature:str) -> bool:
     '''
-    da sistemare, non serve fare tutti e 4 i controlli alla volta ogni volta, (ricreare la signature 
-    e contattare il Db gestionale richiede tempo...) meglio eseguirli in cascata per risparmiare risorse, 
-    e se uno non passa allora return False
+        Funzione che effettua in cascata tutti i controlli necessari 
+        a verificare la validità della richiesta ricevuta
     '''
-    c1 = timestampCheck(ricezione)
-    c2 = signatureCheck()
-    c3 = palestraIdCheck(gestore)
-    c4 = cardIdCheck(gestore)
+    if timestampCheck(ricezione, timestamp):
+        if signatureCheck(idSmartCard, idPalestra, timestamp, signature):
+            if palestraIdCheck(gestore, idPalestra):
+                if cardIdCheck(gestore, idSmartCard):
+                    return True
+    return False
 
-    if c1 and c2 and c3 and c4:
+def smartcardCorretto(idSmartCard:str) -> bool:
+    '''
+        Funzione che controlla la correttezza del campo SmartCardID:
+        6 caratteri esadecimali uppercase
+    '''
+    if len(idSmartCard)==6 and all(c in HEXDIGITS for c in idSmartCard):
         return True
     else:
         return False
     
-def hashing(string):
+def finalHash(idSmartCard:str, timestamp:str):
+    '''Funzione che calcola la signature finale con cui rispondere al client'''
+    string = idSmartCard + timestamp + API_KEY
     return sha256(string.encode('utf-8')).hexdigest()
-
-def timeDifferenceCalculator(timestamp, ricezione):
-    '''
-    Si può invece usare il metodo "datetime.strptime(date_string, format)" per creare un oggetto datetime dalla stringa
-    di timestamp, e sottrarlo con ricezione. Il risultato sarebbe un oggetto "timedelta", dal quale puoi ricavare
-    il numero di secondi di differenza con il medoto "total_seconds". Il codice dovrebbe venire molto più pulito così
-    '''
-    timestamp = datetime.strptime(timestamp,r"%Y-%m-%d %H:%M:%S")
-    delta = ricezione - timestamp
-    return delta.total_seconds()
-    '''
-    #ricostruisce i datetime in un formato col quale si possono sottrarre e calcola i secondi
-    timestampDiviso = timestamp.split(' ')
-    ricezioneDiviso = ricezione.split(' ')
-    timestampData = timestampDiviso[0]
-    timestampoOra = timestampDiviso[1]
-    ricezioneData = ricezioneDiviso[0]
-    ricezioneOra = ricezioneDiviso[1]
-    td = timestampData.split('-')
-    rd = ricezioneData.split('-')
-    to = timestampoOra.split(':')
-    ro = ricezioneOra.split(':')
-    a = datetime(int(td[0]), int(td[1]), int(td[2]), int(to[0]), int(to[1]), int(to[2]))
-    b = datetime(int(rd[0]), int(rd[1]), int(rd[2]), int(ro[0]), int(ro[1]), int(ro[2]))
-    res = int((a - b).total_seconds() // 60)
-    return res'''
 
 
 app = Flask(__name__)
@@ -140,74 +117,64 @@ app = Flask(__name__)
 def home():
     logger.debug("Connessione stabilita")
     data = request.json
-    '''
-    Le variabili globali sono un problema in questo contesto: essendo leggibili da tutto il codice, se una
-    funzione modifica lo stato di una variabile mentre essa viene letta da un'altra, si creano dei problemi
-    (situazione possibile, nel contesto di due o più richieste contemporanee al server)
-    '''
-    global idSmartCard
-    global idPalestra
-    global timestamp
-    global signature
 
     ricezione = datetime.now()
     gestore = Gestionale(DATABASE_GS_URI)
 
-    idSmartCard = data["IDSmartCard"]
-    idPalestra = data["IDPalestra"]
-    timestamp = data["Timestamp"]
-    signature = data["Signature"]
-
-    if check(gestore, ricezione):
+    #controlla che i dati siano nel formato richiesto
+    try:
+        idSmartCard = data["IDSmartCard"]
+        if not smartcardCorretto(idSmartCard):
+            raise ValueError("SmartCard ID non ha un formato accettabile")
+        idPalestra = int(data["IDPalestra"])
+        timestamp = datetime.strptime(data["Timestamp"], r"%Y-%m-%d %H:%M:%S")
+        signature = data["Signature"]
+    except ValueError as e:
+        logger.error("Dati ricevuti incorretti: %s", str(e))
+        return jsonify({"valido": False,
+                        "signature": finalHash(data["IDSmartCard"], data["Timestamp"])})
+    
+    abbonamentoValido = False
+    if check(gestore, ricezione, idSmartCard, idPalestra, timestamp, signature):
         logger.debug("Check passed")
-        #fai rihicesta a selet_abbonamenti
+
+        #fa richicesta a selet_abbonamenti,
+        #se la smart card ricevuta ha un abbonamento valido associato continuano le operazioni
         abbonamenti = gestore.select_abbonamenti(idSmartCard)
-        abbonamentoValido = False
         oggi = date.today()
         for x in abbonamenti:
             if x.valido_dal < oggi < x.valido_al:
                 abbonamentoValido = True
         
         if abbonamentoValido:
-            logger.debug("validosss")
             cliente = gestore.select_client(idSmartCard)
-            pal = gestore.select_palestra(idPalestra)
 
             #pseudonimizzazione id
-            pseudoSmartID = pseudonimizzatore(idSmartCard, encrypt_key=ENCRYPTKEY, pseudo_pad=ENCRYPTPAD)
+            pseudoSmartID = pseudonimizzatore(idSmartCard,
+                                              encrypt_key=ENCRYPTKEY,
+                                              pseudo_pad=ENCRYPTPAD)
 
             #anonimizzazione dati gestionale
             dati = anonimizzatore({
             "sesso":cliente.sesso,
             "data_nascita":cliente.data_nascita,
             "palestra_id":idPalestra,
-            "timestamp":datetime.strptime(timestamp,r"%Y-%m-%d %H:%M:%S")})
+            "timestamp":timestamp})
 
             #salvo logs e statistiche
             magazziniere = LogsAndStats(DATABASE_LS_URI)
-            magazziniere.insert_log(smart_card_id=pseudoSmartID, palestra_id=idPalestra, timestamp=timestamp)
-            magazziniere.insert_stats(sesso=dati["sesso"], fascia_eta=dati["fascia_eta"], palestra_id=dati["palestra_id"],
-                                  data_ingresso=dati["data_ingresso"], fascia_oraria=dati["fascia_oraria"])
-        
-        #da ritornare a client
-        string = str(idSmartCard) + str(timestamp) + API_KEY
-        hashResult = hashing(string)
-        return jsonify({"valido": abbonamentoValido, "signature": hashResult})
-    else: 
-        logger.debug("NOPE")
-        return jsonify({"check": "fallito"})
+            magazziniere.insert_log(smart_card_id=pseudoSmartID,
+                                    palestra_id=idPalestra,
+                                    timestamp=timestamp)
+            magazziniere.insert_stats(sesso=dati["sesso"],
+                                      fascia_eta=dati["fascia_eta"],
+                                      palestra_id=dati["palestra_id"],
+                                      data_ingresso=dati["data_ingresso"],
+                                      fascia_oraria=dati["fascia_oraria"])
+    
+    logger.debug("Abbonamento Valido: %s", abbonamentoValido)
+    return jsonify({"valido": abbonamentoValido,
+                    "signature": finalHash(idSmartCard, timestamp.strftime(DATETIME_FORMAT))})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000) # Da cambiare con la porta 443 a progetto finito
-    '''
-    Da chiedere al Prof: Per il certificato vogliamo usare un container certbot che richieda il certificato 
-    e lo salvi in un volume. Domanda: carichiamo il certificato SSL direttamente nella API o lo affidiamo ad
-    un reverse proxy che si occupi lui di crittografare le comunicazioni con il client?
-
-    Per caricardo direttamente qui nella API, ho trovato questo esempio di codice, ovviamente da modificare:
-    context = SSL.Context(SSL.TLSv1_2_METHOD)
-    context.use_privatekey_file('/etc/letsencrypt/live/your-api-domain.com/privkey.pem')
-    context.use_certificate_file('/etc/letsencrypt/live/your-api-domain.com/fullchain.pem')
-    if __name__ == '__main__':
-       app.run(ssl_context=context, host='0.0.0.0', port=443)
-    '''
+    app.run()
