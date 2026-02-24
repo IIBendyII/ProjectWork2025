83dd83b97fb9e74cad3a585aee4961455b4521d1
Riccardo <riccardo.bandi.studio@fitstic-edu.com>
2026-02-24 09:43:27 +0100

    implementa cpu count per workers

--- api/gunicorn.conf.py
+++ api/gunicorn.conf.py
@@ -1,5 +1,7 @@
+import multiprocessing
+
 bind = "0.0.0.0:8000" # Da cambiare porta 8000 con la porta 443 a progetto finito
-workers = 5 # Per Gunicord documentation: workers = (2 × CPU cores) + 1.
+workers = multiprocessing.cpu_count() * 2 + 1 # Per Gunicorn documentation: workers = (2 × CPU cores) + 1.
 threads = 2 # Threads aggiunti nel caso il DB Gestionale impieghi tempo a rispondere
 worker_tmp_dir = "/tmp"
 log_file = "-"
